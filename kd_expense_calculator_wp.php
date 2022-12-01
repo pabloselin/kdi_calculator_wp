e5fca7a11e4c4601215143ee70b47532a72b1929
Pablo Selin Carrasco Armijo <pabloseline@gmail.com>
2022-12-01 16:28:46 +0100

    ajustes y nuevo build

--- kd_expense_calculator_wp.php
+++ kd_expense_calculator_wp.php
@@ -9,13 +9,13 @@ Author URI: http://pablosel.in
 License: GPL2
 */
 
-define('KD_PLUGIN_VERSION', '0.2.1');
+define('KD_PLUGIN_VERSION', '0.2.3');
 
 function kd_enqueue_scripts() {
     //Main script
     wp_enqueue_script( 'kd_expense_calculator', plugins_url('build/static/js/kd_expense_calculator.js', __FILE__ ), array(), KD_PLUGIN_VERSION, true );
-    wp_enqueue_script( 'kd_expense_calculator_wp', plugins_url('dist/kd_expense_calculator_wp_v022.js', __FILE__ ), array(), KD_PLUGIN_VERSION, true );
-    wp_enqueue_style( 'kd_expense_calculator', plugins_url('dist/kd_expense_calculator_wp_v022.css', __FILE__ ), array(), KD_PLUGIN_VERSION, 'all' );
+    wp_enqueue_script( 'kd_expense_calculator_wp', plugins_url('dist/kd_expense_calculator_wp_v023.js', __FILE__ ), array(), KD_PLUGIN_VERSION, true );
+    wp_enqueue_style( 'kd_expense_calculator', plugins_url('dist/kd_expense_calculator_wp_v023.css', __FILE__ ), array(), KD_PLUGIN_VERSION, 'all' );
 }
 
 add_filter( 'wp_enqueue_scripts', 'kd_enqueue_scripts' );
